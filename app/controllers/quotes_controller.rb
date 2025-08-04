16e349f4ce933028ebe6105b8ccd81b32b1bd363
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-04 10:37:49 +0300

    Update included:   - Add the Quote controller actions for Edit, Create, Updateand Destroy   - Ensure hat all the controller actions to be functioning as intended

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,50 @@
 class QuotesController < ApplicationController
   before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+
+    if @quote.save
+      redirect_to quotes_path, notice: "Quote was created succesfully"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: "Quote was updated succesfully"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @quote.destroy
+        redirect_to quotes_path, notice: "Quote was deleted successfully."
+    else
+        error_message = @quote.errors.full_messages.to_sentence.presence || "There was an error deleting the quote."
+        redirect_to quotes_path, alert: error_message
+    end
+    rescue => e
+      logger.error "Destroy failed: #{e.message}"
+      redirect_to quotes_path, alert: "An unexpected error occurred: #{e.message}"
+  end
+
   private
 
   def set_quote
