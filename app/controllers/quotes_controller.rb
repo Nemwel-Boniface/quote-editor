4adaba4eae77f8d756854a20ec1914bfd09077ca
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-04 12:25:47 +0300

    Add appropriate status for when creating/ updating a quote in the project. Error message shown to the user

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,7 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: "Quote was created succesfully"
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,7 +29,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was updated succesfully"
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
