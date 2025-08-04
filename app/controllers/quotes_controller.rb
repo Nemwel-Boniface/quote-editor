561459e3f4fe92fe22811c65015b322fe84e1a69
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-08-04 10:22:22 +0300

    Add set_quote and qoute_params actions in the Quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,13 @@
 class QuotesController < ApplicationController
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+
+  def quote_params
+    params.require(:quote).permit(:name)
+  end
 end
